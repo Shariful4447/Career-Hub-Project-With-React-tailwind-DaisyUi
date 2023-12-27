db1387fcc0a466d4131879834eb7318c6d9ff696
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-27 07:05:27 +0100

    card design full done

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,17 +1,20 @@
+import { IoLocationOutline } from "react-icons/io5";
+import { PiCurrencyCircleDollar } from "react-icons/pi"
 
 
 const Job = ({job}) => {
     const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
     return (
-        <div className="card card-compact bg-base-100 shadow-xl">
+        <div className="card card-compact bg-base-100 shadow-xl my-10">
         <figure><img className="content-start" src={logo} alt="Shoes" /></figure>
         <div className="card-body text-left">
             <h2 className="card-title">{job_title}</h2>
             <p>{company_name}</p>
             
             <div><button className="text-m border border-[#7E90FE] rounded font-extrabold px-5 py-2 ">{remote_or_onsite}</button><button className="text-m border border-[#7E90FE] rounded m-2 font-extrabold px-5 py-2">{job_type}</button></div>
-            <div>
-                <p>Location: {location} <span className="m-2">Salary: {salary}</span></p> 
+            <div className="flex">
+                <h2 className="flex mr-4"><IoLocationOutline className="text-2xl" /> {location}</h2> 
+                <h2 className="flex"><PiCurrencyCircleDollar className="text-2xl" />Salary : {salary} </h2>
                 
             </div>
             <div className="card-actions ">
