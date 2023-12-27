1c8a4f3d5434bb830b4e50d9ccf0776e72da27cd
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-27 04:31:13 +0100

    show logo from fake data

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
@@ -12,6 +13,12 @@ const FeaturedJobs = () => {
         <div>
             <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+            <div>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
