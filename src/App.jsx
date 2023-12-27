9696850ac1fbbe2cbfdbe58334d6a4e783f80a24
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-27 03:00:04 +0100

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
