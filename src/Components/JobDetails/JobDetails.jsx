4cacd4021054c6911378904c80d2541fd480c2af
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-28 08:01:32 +0100

    job details page data call done

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -10,16 +10,16 @@ const JobDetails = () => {
     console.log(job);
     return (
         <div>
-            <h2 className="text-center">All details here {id}</h2>
+            <h2 className="text-center">Job Details Of :  {job.job_title}</h2>
 
             <div className="grid gap-4 md:grid-cols-4 my-4">
                 <div className="job-details border md:col-span-3">
-                    <h2 className="my-2"><span className="text-xl">Job Description : </span>A UI/UX (User Interface/User Experience) designer is responsible for designing and creating engaging and effective interfaces for software and web applications. This includes designing the layout, visual design, and interactivity of the user interface.</h2>
-                    <h2 className="my-2"><span className="text-xl">Job Responsibility : </span>Collaborating with cross-functional teams: UI/UX designers often work closely with other teams, including product management, engineering, and marketing, to ensure that the user interface is aligned with business and technical requirements. You will need to be able to effectively communicate your design ideas and gather feedback from other team members.</h2>
+                    <h2 className="my-2"><span className="text-xl">Job Description : </span>{job.job_description}</h2>
+                    <h2 className="my-2"><span className="text-xl">Job Responsibility : </span>{job.job_responsibility}</h2>
                     <h2 className="text-xl my-2">Educational Requirements:</h2>
-                    <p>Bachelor degree to complete any reputational university.</p>
+                    <p>{job.educational_requirements}</p>
                     <h2 className="text-xl my-2">Experiences:</h2>
-                    <p>2-3 Years in this field.</p>
+                    <p>{job.experiences}</p>
 
                 </div>
                 <div className="apply-job border p-4">
@@ -28,14 +28,14 @@ const JobDetails = () => {
 
                         <div className="m-2">
                             <h2>Job details</h2>
-                            <h2>Salary</h2>
-                            <h2>Job Title </h2>
+                            <h2>Salary : {job.salary}</h2>
+                            <h2>Job Title : {job.job_title} </h2>
                         </div>
                         <div className="p-2">
                             <h2>Contact Information</h2>
-                            <h2>Phone : </h2>
-                            <h2>Email :</h2>
-                            <h2>Address :</h2>
+                            <h2>Phone : {job.contact_information.phone} </h2>
+                            <h2>Email : {job.contact_information.email}</h2>
+                            <h2>Address : {job.contact_information.address}</h2>
                         </div>
                     </div>
                     <div  className="border-dotted border-2"></div>
